4fd46c2e0d768d38d890952e19735fe31b69f510
Evan Prodromou <eprodromou@wikimedia.org>
2020-04-06 14:04:56 -0400

    Output CSS data into the SVG file

--- covid19databot.py
+++ covid19databot.py
@@ -24,7 +24,8 @@ import csv
 import io
 
 WORLD_MAP_TEMPLATE = "File:BlankMap-World.svg"
-WORLD_MAP_FILE = "BlankMap-World.svg"
+WORLD_MAP_TEMPLATE_FILE = "BlankMap-World.svg"
+WORLD_MAP_DATA_FILE = 'Covid19DataBot-Case-Data-World.svg'
 
 WORLD_CASE_DATA_URL_TEMPLATE = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/%s.csv"
 
@@ -207,7 +208,17 @@ ISO_CODES = {
     'Australia': 'AU',
     'Canada': 'CA',
     'China': 'CN',
+    'United States': 'US',
     'US': 'US'}
+
+COLOUR_CODES = {
+    '1-9': 'e0e0e0',
+    '10-99': 'ffc0c0',
+    '100-999': 'ee7070',
+    '1000-9999': 'c80200',
+    '10000-99999': '900000',
+    '100000+': '510000'
+}
 
 def main():
     "Main function for the COVID-19 data bot"
@@ -222,14 +233,13 @@ def updateWorldCaseDataMap():
     downloadWorldMapTemplate()
     data = downloadWorldCaseData()
     country_data = processWorldCaseData(data)
-    print(country_data)
     createWorldCaseDataMap(country_data)
     uploadWorldCaseDataMap()
 
 def downloadWorldMapTemplate():
     site = pywikibot.Site('commons', 'commons')
     image = pywikibot.FilePage(site, WORLD_MAP_TEMPLATE)
-    image.download(WORLD_MAP_FILE)
+    image.download(WORLD_MAP_TEMPLATE_FILE)
 
 def downloadWorldCaseData():
     today = datetime.date.today()
@@ -250,16 +260,72 @@ def processWorldCaseData(data):
     d = csv.DictReader(f)
     countries = {}
     for row in d:
-        if not row['FIPS'] and not row['Admin2'] and not row['Province_State']:
-            iso = nameToISO(row['Country_Region'])
-            countries[iso] = {'confirmed': int(row['Confirmed']), 'deaths': int(row['Deaths']), 'recovered': int(row['Recovered'])}
+        iso = nameToISO(row['Country_Region'])
+        if iso and len(iso) > 0:
+            country = countries.get(iso, {'confirmed': 0, 'deaths': 0, 'recovered': 0})
+            country['confirmed'] += int(row['Confirmed'])
+            country['deaths'] += int(row['Deaths'])
+            country['recovered'] += int(row['Recovered'])
+            countries[iso] = country
+          
     return countries
 
 def nameToISO(name):
     return ISO_CODES.get(name)
 
 def createWorldCaseDataMap(country_data):
-    pass
+    class_members = createClassMembers(country_data)
+    css = classMembersToCSS(class_members)
+    injectCSS(css)
+
+def createClassMembers(country_data):
+    class_members = {
+        '1-9': [],
+        '10-99': [],
+        '100-999': [],
+        '1000-9999': [],
+        '10000-99999': [],
+        '100000+': []
+    }
+
+    for code, data in country_data.items():
+        cases = data['confirmed']
+        key = code.lower()
+        if cases >= 100000:
+            class_members['100000+'].append(key)
+        elif cases <= 99999 and cases >= 10000:
+             class_members['10000-99999'].append(key)
+        elif cases <= 9999 and cases >= 1000:
+             class_members['1000-9999'].append(key)
+        elif cases <= 999 and cases >= 100:
+             class_members['100-999'].append(key)
+        elif cases <= 99 and cases >= 10:
+             class_members['10-99'].append(key)
+        elif cases <= 9 and cases >= 1:
+             class_members['1-9'].append(key)
+
+    return class_members
+
+def classMembersToCSS(class_members):
+    css = ''
+    for band, members in class_members.items():
+        css += classCSS(band, members)
+    return css
+
+def classCSS(band, members):
+    colour = COLOUR_CODES[band]
+    dotted = map(lambda x: "." + x, members)
+    css = ", ".join(dotted) + " {\n    fill:#" + colour + ";\n}\n"
+    return css
+
+def injectCSS(css):
+    data = None
+    with open(WORLD_MAP_TEMPLATE_FILE, 'r') as file:
+        data = file.read()
+    data = data.replace('</style>', css + '\n</style>')
+    with open(WORLD_MAP_DATA_FILE, 'w') as file:
+        file.write(data)
+
 
 def uploadWorldCaseDataMap():
     pass
